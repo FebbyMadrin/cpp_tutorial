c08a1f7269b8e31d60ca4d6862d618eeffc73b8d
febbymadrin <febbymadrin@gmail.com>
2021-03-29 15:36:58 +0200

    add more example

--- dasar/main.cpp
+++ dasar/main.cpp
@@ -1,13 +1,19 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T add (T value1, T value2){
+    qInfo()<< value1+value2;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    do {
-
-    }while(true);
-
+    add(100,8);
 
     return a.exec();
 }
